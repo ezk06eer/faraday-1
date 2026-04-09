d9e3768235cbcbf3efdc9df6f0a184bd82b64df4
David Kraus <dkraus@faradaysec.com>
2026-04-09 16:09:24 -0300

    Add migration

---
+++ faraday/migrations/versions/a94bf314d524_remove_is_preview.py
@@ -0,0 +1,24 @@
+"""remove is_preview
+
+Revision ID: a94bf314d524
+Revises: a9b75b0ddc01
+Create Date: 2026-04-08 15:52:33.048551+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a94bf314d524'
+down_revision = 'a9b75b0ddc01'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column('executive_report', 'is_preview')
+
+
+def downgrade():
+    op.add_column('executive_report', sa.Column('is_preview', sa.BOOLEAN(), server_default=sa.text('false'), autoincrement=False, nullable=False))

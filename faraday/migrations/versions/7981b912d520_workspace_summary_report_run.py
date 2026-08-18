3eca17a09c6a4a36a7257980606526e73433526f
David Kraus <dkraus@faradaysec.com>
2026-08-18 18:05:30 -0300

    Fix WSRR table creation

--- faraday/migrations/versions/7981b912d520_workspace_summary_report_run.py
+++ faraday/migrations/versions/7981b912d520_workspace_summary_report_run.py
@@ -21,6 +21,7 @@ def upgrade():
         sa.Column('update_date', sa.DateTime(), nullable=True),
         sa.Column('id', sa.Integer(), nullable=False),
         sa.Column('workspace_summary_report_id', sa.Integer(), nullable=False),
+        sa.Column('filename', sa.Text(), nullable=False),
         sa.Column('creator_id', sa.Integer(), nullable=True),
         sa.Column('update_user_id', sa.Integer(), nullable=True),
         sa.ForeignKeyConstraint(['creator_id'], ['faraday_user.id'], ondelete='SET NULL'),
